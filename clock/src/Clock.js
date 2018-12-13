4623722ca304a68626ea4d764520149e3db0f610
ThaoNt <thaont@rikkeisfot.com>
2018-12-13 17:30:01 +0700

    clock done

--- clock/src/Clock.js
+++ clock/src/Clock.js
@@ -2,19 +2,51 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.getTime();
+  }
+  componentDidMount() {
+    this.setTimer();
+  }
+
+  componentWillUnmount() {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+  }
+
+  setTimer() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.updateClock.bind(this), 1000);
+    // console.log(0);
+  }
+
+  updateClock() {
+    this.setState(this.getTime, this.setTimer);
+  }
+
+  getTime() {
+    const currentTime = new Date();
+    return {
+      hours: currentTime.getHours(),
+      minutes: currentTime.getMinutes(),
+      seconds: currentTime.getSeconds(),
+      ampm: currentTime.getHours() >= 12 ? "pm" : "am"
+    };
+  }
+
   render() {
-    const currentTime = new Date(),
-      hours = currentTime.getHours(),
-      minutes = currentTime.getMinutes(),
-      seconds = currentTime.getSeconds(),
-      ampm = hours >= 12 ? "pm" : "am";
+    const { hours, minutes, seconds, ampm } = this.state;
 
     return (
       <div className="clock">
-        {hours == 0 ? 12 : hours > 12 ? hours - 12 : hours}:
+        {hours === 0 ? 12 : (hours > 12) ? hours - 12 : hours}:
         {minutes > 9 ? minutes : `0${minutes}`}:
         {seconds > 9 ? seconds : `0${seconds}`} {ampm}
       </div>
     );
   }
 }
+
+export default Clock;
